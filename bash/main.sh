46b7a9652e9b91435abb67cfae072b04c5040dd4
Jordi Villar <jrdi.villar@gmail.com>
2013-10-18 21:14:17 +0200

    add LC_ALL & load rbenv

--- bash/main.sh
+++ bash/main.sh
@@ -1,3 +1,4 @@
+export LC_ALL="en_US.UTF-8"
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
 export DOTFILES=~/Code/dotfiles
@@ -8,5 +9,7 @@ source "$DOTFILES/bash/ps1.sh"
 if [ -s "$DOTFILES/bash/private.sh" ]
   then source "$DOTFILES/bash/private.sh"
 fi
+
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
 export PS1='\[\033[01;33m\]$(__rbenv_ps1) \[\033[01;37m\]\w\[\033[01;33m\]$(__git_ps1 "[%s]") \[\033[01;37m\]\$\[\033[00m\] '
