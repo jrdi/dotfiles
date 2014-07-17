253d9cdae8e2c4e3b6163647e20a8803d78e49cc
Jordi Villar <jrdi.villar@gmail.com>
2014-07-17 11:57:20 +0200

    set theme as default

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@ ln -s ~/Code/dotfiles/bash/main.sh ~/.bash_profile
 ln -s ~/Code/dotfiles/gitconfig ~/.gitconfig
 
 open ~/Code/dotfiles/resources/Jordi.terminal
+sleep 1 # Wait a bit to make sure the theme is loaded
+defaults write com.apple.terminal 'Default Window Settings' -string 'paulmillr'
+defaults write com.apple.terminal 'Startup Window Settings' -string 'paulmillr'
 
 . ~/.bash_profile
