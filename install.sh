225e52ba9ece86b985e385e38604336b732f91d9
Jordi Villar <jrdi.villar@gmail.com>
2014-07-20 19:44:01 +0200

    rename symlinks

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-ln -s ~/Code/dotfiles/bash/main.sh ~/.bash_profile
-ln -s ~/Code/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/Code/dotfiles/bash/main.symlink ~/.bash_profile
+ln -sf ~/Code/dotfiles/gitconfig.symlink ~/.gitconfig
 
 open ~/Code/dotfiles/resources/Jordi.terminal
 sleep 1 # Wait a bit to make sure the theme is loaded
-defaults write com.apple.terminal 'Default Window Settings' -string 'paulmillr'
-defaults write com.apple.terminal 'Startup Window Settings' -string 'paulmillr'
+defaults write com.apple.terminal 'Default Window Settings' -string 'Jordi'
+defaults write com.apple.terminal 'Startup Window Settings' -string 'Jordi'
 
 . ~/.bash_profile
